f491a937f2bb5a5c5d2d0dbd4749d4ed7e9474e9
Md. Ahasan Habib <27882232+habibcse009@users.noreply.github.com>
2020-12-26 01:24:17 +0600

    connect database

---
+++ app/src/main/java/com/project/eevent/remote/ApiClient.java
@@ -0,0 +1,27 @@
+package com.project.eevent.remote;
+
+
+
+import com.project.eevent.Constant;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    public static final String BASE_URL = Constant.BASE_URL;
+    public static Retrofit retrofit = null;
+
+    public static Retrofit getApiClient() {
+
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+
+    }
+
+}
