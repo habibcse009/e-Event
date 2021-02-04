584656f5a56f154bce6dd969f8405d44bb3ce889
Md. Ahasan Habib <27882232+habibcse009@users.noreply.github.com>
2021-02-04 20:58:06 +0600

    update project, fixed bug

--- app/src/main/java/com/project/eevent/Photographer/Product/ProductDescriptionPhotographerActivity.java
+++ app/src/main/java/com/project/eevent/Photographer/Product/ProductDescriptionPhotographerActivity.java
@@ -99,7 +99,8 @@ public class ProductDescriptionPhotographerActivity extends AppCompatActivity {
         txtOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ProductDescriptionPhotographerActivity.this, OrderPhotographerActivity.class);
+                //egula comment hobe na
+           /*     Intent intent = new Intent(ProductDescriptionPhotographerActivity.this, OrderPhotographerActivity.class);
                 intent.putExtra("name", name);
                 intent.putExtra("price", price);
                 intent.putExtra("category", category);
@@ -108,7 +109,7 @@ public class ProductDescriptionPhotographerActivity extends AppCompatActivity {
                 intent.putExtra("id", id);
 
                 startActivity(intent);
-
+*/
             }
         });
 
